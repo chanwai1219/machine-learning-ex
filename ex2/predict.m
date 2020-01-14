f808b9b6399ea14c20b7d282ee68564d90d50e57
Wei CHEN <weichen@astri.org>
2020-01-14 16:51:09 +0800

    ex2

--- ex2/predict.m
+++ ex2/predict.m
@@ -15,11 +15,10 @@ p = zeros(m, 1);
 %               You should set p to a vector of 0's and 1's
 %
 
+h = sigmoid(X * theta);
+threshold = 0.5;
 
-
-
-
-
+p = h >= threshold;
 
 % =========================================================================
 
