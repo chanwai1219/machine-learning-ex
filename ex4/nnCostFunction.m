2a104bdd7a8778fb11910c968823dca27b77ef80
Wei CHEN <weichen@astri.org>
2020-01-27 20:58:14 +0800

    ex4

--- ex4/nnCostFunction.m
+++ ex4/nnCostFunction.m
@@ -62,23 +62,52 @@ Theta2_grad = zeros(size(Theta2));
 %               and Theta2_grad from Part 2.
 %
 
+X = [ones(m, 1), X];
 
+tmp = zeros(m, num_labels);
+for i = 1:length(y)
+    tmp(i, y(i)) = 1;
+end
+y = tmp;
 
+tmp1 = Theta1(:, 2:end);
+tmp2 = Theta2(:, 2:end);
+reg = (lambda / (2 * m)) * (sum(sum(tmp1.^2)) + sum(sum(tmp2.^2)));
 
+a1 = X;                             % a1 -> 5000 x 401
+z2 = a1 * Theta1';                  % z2 -> 5000 x 25
+a2 = sigmoid(z2);
+a2 = [ones(size(a2, 1), 1), a2];    % a2 -> 5000 x 26
+z3 = a2 * Theta2';                  % z3 -> 5000 x 10
+h = sigmoid(z3);                    % h  -> 5000 x 10
 
+%for i = 1:m
+%    J = J - log(h(i, :)) * y(:, i) - log(1 - h(i, :)) * (1 - y(:, i));
+%end
 
+J = -log(h) * y' - log(1 - h) * (1 - y');
+J = sum(sum(diag(J))) / m;
+J = J + reg;
 
+%for i = 1:m
+%    delta3 = h(i, :) - y(i, :); % delta3 -> 1 x 10
+%    delta2 = (delta3 * tmp2) .* sigmoidGradient(z2(i, :)); % delta2 -> 1 x 25
+%    Theta2_grad = Theta2_grad + delta3' * a2(i, :);
+%    Theta1_grad = Theta1_grad + delta2' * a1(i, :);
+%end
 
+delta3 = h - y;
+delta2 = (delta3 * tmp2) .* sigmoidGradient(z2);
+Theta2_grad =  delta3' * a2;
+Theta1_grad =  delta2' * a1;
 
+Theta1_grad = Theta1_grad / m;
+Theta2_grad = Theta2_grad / m;
 
-
-
-
-
-
-
-
-
+grad_reg1 = [zeros(size(Theta1, 1), 1) (lambda / m) * tmp1];
+grad_reg2 = [zeros(size(Theta2, 1), 1) (lambda / m) * tmp2];
+Theta1_grad = Theta1_grad + grad_reg1;
+Theta2_grad = Theta2_grad + grad_reg2;
 
 % -------------------------------------------------------------
 
